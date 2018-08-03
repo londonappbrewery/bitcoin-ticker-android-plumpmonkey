44301a0aa5c3d3b596bb19223af191dba4412a19
Jon White <white.jon@gmail.com>
2018-08-03 19:21:37 +0100

    Added Spinner listener.

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -45,7 +45,17 @@ public class MainActivity extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
         // TODO: Set an OnItemSelected listener on the spinner
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Bitcoin", "onItemSelected() - " + parent.getItemAtPosition(position));
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.d("Bitcoin","onNothingSelected()");
+            }
+        });
     }
 
     // TODO: complete the letsDoSomeNetworking() method
